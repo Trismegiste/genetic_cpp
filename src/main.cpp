c12481839444118e6e4ed27a21ff2a7b9dd55e2c
Trismegiste <flo@trismegiste.net>
2023-09-17 21:27:48 +0200

    stopwatch before optim

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,25 @@
 #include "FreeEcosystem.h"
 #include "TextLogger.h"
 #include <iostream>
+#include <ctime>
 
 int main() {
-  int maxGeneration = 100;
-  int popSize = 1000;
-  float extinctRatio = 0.01;
-  int battleRound = 9;
+    int maxGeneration = 3;
+    int popSize = 200;
+    float extinctRatio = 0.01;
+    int battleRound = 9;
 
-  CharacterFactory factory;
-  TextLogger logger(extinctRatio);
-  FreeEcosystem world(popSize, factory, logger);
+    CharacterFactory factory;
+    TextLogger logger(extinctRatio);
+    FreeEcosystem world(popSize, factory, logger);
 
-  for (int generation = 0; generation < maxGeneration; generation++) {
-    std::cout << "==Generation " << generation << "==" << std::endl;
-    world.evolve(battleRound, extinctRatio);
-  }
+    const std::clock_t c_start = std::clock();
+    for (int generation = 0; generation < maxGeneration; generation++) {
+        std::cout << "==Generation " << generation << "==" << std::endl;
+        world.evolve(battleRound, extinctRatio);
+    }    
+    const std::clock_t c_end = std::clock();
+    std::cout << "Chrono=" << 1.0 * (c_end - c_start) / CLOCKS_PER_SEC << std::endl;
 
-  return 0;
+    return 0;
 }
